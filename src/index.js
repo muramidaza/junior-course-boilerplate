551f065a39fc7a73181793bb4f1b40d8798943ef
muramidaza <muramidaza@gmail.com>
2020-10-10 21:35:40 +0300

    first task version 2

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './index.css';
+
 import data from './products.json';
 
-import './index.css';
+let products = data.slice(0, 3);
 
 class GoodItem extends React.Component {
 	render() {
-		return (<li> {this.props.good.name} </li>);
+		return (<li className="goodItem"> {this.props.good.name} </li>);
 	}
 }
 
 class GoodsList extends React.Component {
 	render() {
 		return (
-				<div className="goodslist">
-					<h2>Goods list</h2>
-					<ul>
-						{data.slice(0, 3).map(good => (<GoodItem className="gooditem" key={good.id} good={good}/>))}
-					</ul>
-				</div>
-			)
-		}
+			<div className="goodsList">
+				<h2>Goods list</h2>
+				<ul>
+					{products.map(good => (<GoodItem key={good.id} good={good}/>))}
+				</ul>
+			</div>
+		)
+	}
 }
 
 function App() {

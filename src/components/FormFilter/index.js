f14ea1cf32523c2d3bd7291f174e94d2b2e0accf
muramidaza <muramidaza@gmail.com>
2020-10-21 20:22:55 +0300

    task 4 stage 2 add mask

--- src/components/FormFilter/index.js
+++ src/components/FormFilter/index.js
@@ -40,8 +40,8 @@ class FormFilter extends logComponent {
 				<div>
 					<p>Цена:</p>
 					<div>
-						от <InputNumber minValue={0} defaultValue={this.props.minPrice} onChangeNumber={this.handleChangeMinPrice}/> 
-						до <InputNumber minValue={0} defaultValue={this.props.maxPrice} onChangeNumber={this.handleChangeMaxPrice}/>
+						от <InputNumber defaultValue={this.props.minPrice} onChangeNumber={this.handleChangeMinPrice}/> 
+						до <InputNumber defaultValue={this.props.maxPrice} onChangeNumber={this.handleChangeMaxPrice}/>
 					</div>
 				</div>
 				<div>
--- src/components/InputNumber/index.js
+++ src/components/InputNumber/index.js
@@ -3,16 +3,38 @@ import {logComponent} from '../../logComponent.js';
 
 import './index.css';
 
+	
+function checkNumber(str) {
+	const reg = new RegExp('^[0-9]+$');
+	return reg.test(str);
+}
+
 class InputNumber extends logComponent {
+	constructor(props) {
+		super(props);
+		
+		this.prevValue = this.props.defaultValue;
+	}
 
 	handleChange = (event) => {
-		this.props.onChangeNumber(+event.target.value);
+		let str = event.target.value;
+		
+		//если инпут пустой - ничего не делаем, если была введена не цифра - возвращаем предыдущее значение в инпут, если введены только натуральное число - то передаем в обработчик
+		if(str.length > 0) {
+			if(checkNumber(str)) {
+				this.prevValue = str;
+				this.props.onChangeNumber(+str);
+			} else {
+				event.target.value = this.prevValue;
+			}
+		}
+		
 		event.preventDefault();
 	}
 		
 	render() {
 		return (
-			<input className="inputNumber" type="number" min={this.props.minValue} defaultValue={this.props.defaultValue} onChange={this.handleChange} />
+			<input className="inputNumber" type="text" defaultValue={this.props.defaultValue} onChange={this.handleChange} />
 		);
 	};
 };

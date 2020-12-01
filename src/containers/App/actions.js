cc95ced429f458991624200838b8a3bdc55af7d9
Predein <muramidaza@gmail.com>
2020-12-01 08:41:40 +0300

    task 9 stage 4

--- src/containers/App/actions.js
+++ src/containers/App/actions.js
@@ -1,31 +1,30 @@
 import {LOAD_DATA_SUCCESS, LOAD_DATA_FAILURE, LOAD_DATA_STARTED} from './types';
-  
+
 export const loadData = () => {
-    return dispatch => {
-        dispatch(loadDataStarted());
-        
-        
-        fetch('https://course-api.csssr.school/products')
-            .then(
-                res => {dispatch(loadDataSuccess(res))}
-            )
-            .catch(
-                err => {dispatch(loadDataFailure(err))}
-            );
-        
-    };
+	return dispatch => {
+		dispatch(loadDataStarted());
+		
+		fetch('https://course-api.csssr.school/products', {mode: 'cors'})
+			.then(
+				res => {dispatch(loadDataSuccess(res))}
+			)
+			.catch(
+				err => {dispatch(loadDataFailure(err))}
+			);
+
+	};
 };
   
 const loadDataSuccess = data => ({
-    type: LOAD_DATA_SUCCESS,
-    payload: {data}
+	type: LOAD_DATA_SUCCESS,
+	payload: {data}
 });
   
 const loadDataStarted = () => ({
-    type: LOAD_DATA_STARTED
+	type: LOAD_DATA_STARTED
 });
 
 const loadDataFailure = error => ({
-    type: LOAD_DATA_FAILURE,
-    payload: {error}
+	type: LOAD_DATA_FAILURE,
+	payload: {error}
 });
--- src/containers/App/reducers.js
+++ src/containers/App/reducers.js
@@ -1,20 +1,20 @@
 import * as types from './types';
   
 const initialState = {
-    loading: false,
-    productsData: [],
-    error: null
+	loading: false,
+	productsData: [],
+	error: null
 };
   
 export default function reducers(state = initialState, action) {
-    switch (action.type) {
-        case types.LOAD_DATA_STARTED:
-            return {...state, loading: true};
-        case types.LOAD_DATA_SUCCESS:
-            return {...state, loading: false, error: null, productsData: action.payload};
-        case types.LOAD_DATA_FAILURE:
-            return {...state, loading: false, error: action.payload.error};
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case types.LOAD_DATA_STARTED:
+			return {...state, loading: true};
+		case types.LOAD_DATA_SUCCESS:
+			return {...state, loading: false, error: null, productsData: action.payload};
+		case types.LOAD_DATA_FAILURE:
+			return {...state, loading: false, error: action.payload.error};
+		default:
+			return state;
+	}
 }

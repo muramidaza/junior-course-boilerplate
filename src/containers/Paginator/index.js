5787e6d2a08d62054d7ee034c857aa403d3f38cf
muramidaza <muramidaza@gmail.com>
2020-12-12 20:36:24 +0300

    task 10 stage 23

--- src/containers/Paginator/index.js
+++ src/containers/Paginator/index.js
@@ -54,7 +54,7 @@ class Paginator extends React.Component {
 
 					{this.arrPageNumbers[this.state.section].map((item, i) => {
 						const pathname =
-							'/catalog/' + this.props.selectedCategory + '/' + i;
+							'/catalog/' + this.props.selectedCategory + '/' + item;
 
 						return (
 							<NavLink
@@ -62,7 +62,7 @@ class Paginator extends React.Component {
 								activeClassName="activ"
 								isActive={(match, location) => {
 									if (
-										(!location.pathname.split('/')[3] && i == 0) ||
+										(!location.pathname.split('/')[3] && item == 0) ||
 										location.pathname == pathname
 									)
 										return true;
